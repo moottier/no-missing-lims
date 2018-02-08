cbd0cead45c88b56506f019b5562cc07caf07e5d
moottier <andre.mottier@gmail.com>
2018-02-08 10:01:41 -0800

    fixing gitignore/mysql.js.ex, hiding secrets

--- mysql.js.ex
+++ mysql.js.ex
@@ -8,18 +8,17 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-<<<<<<< HEAD
 connection.query('\
     select * from SAMPLE \
     where STATUS = "INCOMPLETE" or STATUS = "IN_PROGRESS" \
     and SAMPLE_TYPE = "OUTSIDE"', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results);
-=======
+
 connection.query('select * from SAMPLE where STATUS = "INCOMPLETE" and SAMPLE_TYPE = "OUTSIDE"', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
->>>>>>> fa5f280a21f08ecfa33b277b10cb1c30e9ea1193
+
 });
 
 connection.end();
