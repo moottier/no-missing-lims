bafd74286031c5f424ac014acec571e21ee3a84e
moottier <andre.mottier@gmail.com>
2018-02-13 14:49:16 -0800

    fixed import/require from mysql to app

--- mysql.js
+++ mysql.js
@@ -1,29 +1,18 @@
-import pwd from 'config';
+var pwd = require('./config');
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+module.exports = connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'pi',
     password : pwd,
     database : 'cc'
 });
 
-connection.connect();
+connection.connect(function(err){
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 
-connection.query('\
-    select * from SAMPLE \
-    where STATUS = "INCOMPLETE" or STATUS = "IN_PROGRESS" \
-    and SAMPLE_TYPE = "OUTSIDE"', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
-
-connection.query('select * from SAMPLE where STATUS = "INCOMPLETE" and SAMPLE_TYPE = "OUTSIDE"', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-
-connection.query('select * from SAMPLE where STATUS = "INCOMPLETE" and SAMPLE_TYPE = "OUTSIDE"', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+    console.log('connected as id ' + connection.threadId);
 });
-
-connection.end();
