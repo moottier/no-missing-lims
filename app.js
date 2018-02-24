d95f7b29667b5f6829e852207acd47dd618cedf0
moottier <andre.mottier@gmail.com>
2018-02-24 08:22:38 -0800

    modified:   .gitignore 	modified:   app.js required async, using async.parallel to make db queries 	deleted:    bin/www unused boilerplate from setup 	modified:   package-lock.json 	modified:   package.json 	modified:   public/stylesheets/style.less 	renamed:    pyLoader/adder.py -> python/adder.py 	renamed:    pyLoader/datamaker.py -> python/datamaker.py 	renamed:    pyLoader/login.py -> python/login.py 	deleted:    routes/index.js unused boilerplate from setup 	deleted:    routes/users.js unused boilerplate from setup 	modified:   views/index.ejs added autorefresh script

--- app.js
+++ app.js
@@ -2,82 +2,123 @@ var express = require('express');
 var logger = require('morgan');
 var path = require('path');
 var mysql = require('mysql');
+var async = require('async');
 var pwd = require('./config');
 
-module.exports = app = express();
+app = express();
 
 app.use(logger('dev'));
 
-var pool =  mysql.createPool({
-	host: 'localhost',
-	user: 'pi',
-	// REPLACE PASSWORD WITH IMPORTED AND HIDDEN PWD
-	password: pwd,
-	database: 'cc'
-});
-
-// use assets in public directory
+// include 'public' directory (w/o '/path/*' root) in path envir
 app.use(express.static(path.join(__dirname, 'public')));
-
-// set view directory
+// view directory
 app.set('views', path.join(__dirname, 'views'));
-
-
-// set html as default view engine extension
+// html as default view engine extension
 app.set('view engine', 'ejs');
 
+const pool =  mysql.createPool({
+	host: 'localhost',
+	user: 'pi',
+    password: pwd,
+    database: 'cc'
+});
+const queryObj = {
+    queryDailies : 'select R.SAMPLE_NUMBER, SAMPLE.SAMPLE_NAME, R.ENTRY \
+                   from RESULT R inner join SAMPLE \
+                   on R.SAMPLE_NUMBER = SAMPLE.SAMPLE_NUMBER \
+                   where SAMPLE.SAMPLE_TYPE = "DAILY" \
+                   order by ENTRY',
+    queryBottling : 'select R.SAMPLE_NUMBER, R.ENTRY, SAMPLE.SAMPLE_NAME \
+                    from RESULT R inner join SAMPLE \
+                    on R.SAMPLE_NUMBER = SAMPLE.SAMPLE_NUMBER \
+                    where SAMPLE.SAMPLE_TYPE = "BOTTLING" and R.IN_SPEC = "N" \
+                    order by ENTRY',
+    queryOutsides : 'select R.SAMPLE_NUMBER, R.ENTRY, SAMPLE.SAMPLE_NAME \
+                    from RESULT R inner join SAMPLE \
+                    on R.SAMPLE_NUMBER = SAMPLE.SAMPLE_NUMBER \
+                    where SAMPLE.SAMPLE_TYPE = "OUTSIDE" \
+                    order by ENTRY'
+};
 
-function dbQuery(query){
-    var dataHolder = [];
-    pool.getConnection(function(err, connection) {
-	pool.query(query, function (err, rows, fields) {
+function makeSampleObj(data, cb) {
+    //console.log(data);
+    var sampleData = []
+    for (var i = 0; i < data.length; i++) {
+	var sample = {
+	    'SAMPLE_NUMBER' : data[i].SAMPLE_NUMBER,
+	    'SAMPLE_NAME' : data[i].SAMPLE_NAME,
+	    'ENTRY' : data[i].ENTRY
+	}
+	sampleData.push(sample);
+    };
+    if (cb) {
+	//console.log(sampleData);
+	cb(sampleData);
+    } else {
+	return sampleData;
+    }
+};
+function groupDataByEntry(data) {
+    var data = data.reduce( function (acc, init) {
+	if(acc[init['ENTRY']]) {
+	    acc[init['ENTRY']].push( {'SAMPLE_NUMBER': init['SAMPLE_NUMBER'],
+				      'SAMPLE_NAME': init['SAMPLE_NAME']} );
+	} else {
+	    acc[init['ENTRY']] = [ {'SAMPLE_NUMBER': init['SAMPLE_NUMBER'],
+				    'SAMPLE_NAME': init['SAMPLE_NAME']} ];
+	}
+	return acc;
+    }, {});
+    //console.log(data);
+    return data;
+};
+
+async.parallel({
+    dailies: function(renderer) {
+	pool.query(queryObj.queryDailies, function(err, res, fields) {
 	    if (err) {
-		res.status(500).json({'status_code': 500, 'status_message': 'server error'});
+		console.log(err);
 	    } else {
-		for (var i = 0; i < rows.length; i++) {
-		    var sample = {
-			'SAMPLE_NUMBER' : rows[i].SAMPLE_NUMBER,
-			'SAMPLE_NAME' : rows[i].SAMPLE_NAME,
-			'ENTRY' : rows[i].ENTRY
-		    }
-		    dataHolder.push(sample);
-		}
-		connection.release();
-		console.log(dataHolder);
-		return dataHolder;
-	    }    
+		renderer(err, groupDataByEntry(makeSampleObj(res)));
+	    };
+	});
+    },
+    bottling: function(renderer) {
+	pool.query(queryObj.queryBottling, function(err, res, fields) {
+	    if (err) {
+		console.log(err);
+	    } else {
+	    	renderer(err, makeSampleObj(res));
+	    };
+	});
+    },
+    outsides: function(renderer) {
+	pool.query(queryObj.queryOutsides, function(err, res, fields) {
+	    if (err) {
+		console.log(err);
+	    } else {
+	    	renderer(err, makeSampleObj(res));
+	    };
+	});
+    },
+}, function(err, data) {
+    app.get('/', function(req, res){
+	res.render('index', {
+	    title : 'TEST',
+	    dailyData : JSON.stringify(data.dailies),
+	    bottlingLength : data.bottling.length,
+	    outsideLength : data.outsides.length
 	});
     });
-};
-
-app.get('/', function(req, res){
-    var queryDailies =  'select R.SAMPLE_NUMBER, R.ENTRY, SAMPLE.SAMPLE_NAME \
-        from RESULT R inner join SAMPLE \
-        on R.SAMPLE_NUMBER = SAMPLE.SAMPLE_NUMBER \
-        where SAMPLE.SAMPLE_TYPE = "DAILY" \
-        order by ENTRY';
-	    
-    var queryBottling = 'select R.SAMPLE_NUMBER, R.ENTRY, SAMPLE.SAMPLE_NAME \
-	from RESULT R inner join SAMPLE \
-	on R.SAMPLE_NUMBER = SAMPLE.SAMPLE_NUMBER \
-	where SAMPLE.SAMPLE_TYPE = "BOTTLING" and R.IN_SPEC = "N" \
-        order by ENTRY';
-
-    var queryOutsides = 'select R.SAMPLE_NUMBER, R.ENTRY, SAMPLE.SAMPLE_NAME \
-	from RESULT R inner join SAMPLE \
-	on R.SAMPLE_NUMBER = SAMPLE.SAMPLE_NUMBER \
-	where SAMPLE.SAMPLE_TYPE = "OUTSIDE"';
-
-    
-    res.render('index', {
-	title : 'TEST',
-	dailies : dbQuery(queryDailies),
-	bottlings : dbQuery(queryBottling),
-	outsides : dbQuery(queryOutsides)
-    });
+    //console.log(JSON.stringify(data.dailies));
 });
-		    
-if (!module.parent) {
-    app.listen(8081);
-    console.log('express started on 10.0.0.219:8081');
-};
+
+var server = app.listen(8081);
+
+// trying to fix eaddrinuse error when nodemo restarts due to file changes
+//process.on('uncaughtException', (err) => { console.log(err.stack); process.exit(); });
+//process.on('SIGINT', () => {console.log("BYE!"); process.exit(); });
+//require('node-clean-exit') ();
+
+module.exports = app;
+
